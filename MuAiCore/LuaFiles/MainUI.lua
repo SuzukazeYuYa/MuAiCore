f471e3a2b680019695b7ed9d906fa0e1a83004b6
MuAi <543381348@qq.com>
2025-03-25 04:27:36 +0800

    v 189 : 修复更新拉不到文件 创建空白文件问题

--- MuAiCore/LuaFiles/MainUI.lua
+++ MuAiCore/LuaFiles/MainUI.lua
@@ -88,14 +88,6 @@ local DrawMainUI = function(M)
             if GUI:IsItemClicked(0) then
                 M.Config.Main = M.CreateDefMainCfg()
             end
-            GUI:Button("恢复默认设置", 300, 20)
-            if GUI:IsItemClicked(0) then
-                local replacePath = GetStartupPath() .."\\LuaMods"
-                d(replacePath .. "\\TimelineReactions\\Jackpot")
-                if FileExists(replacePath .. "\\TimelineReactions\\Jackpot") then
-                    FileDelete(replacePath .. "\\TimelineReactions\\Jackpot")
-                end
-            end
         end
 
         GUI:SetNextTreeNodeOpened(true, GUI.SetCond_Appearing | GUI.SetCond_Once)
